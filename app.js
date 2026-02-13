83e0213beac500a0bf85286da69dc6fa301d1b07
HanGieTime <elgykputra155@gmail.com>
2026-02-13 20:52:17 +0700

    install ejs, seting ejs, pisah view

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs') // setting penggunan tampate engine untuk express
+app.set('views','./view') //setting enunaan folder untuk menyimpan file .ejs
+
 app.get('/', (req, res) => {
-  res.send(
-      `<h1>Halaman Utama</h1><hr>
-      <p>ini adalah halaman utama kita</p><hr>
-      <button>simpan</button>`
-    )
+  res.render('beranda')
 })
-app.get('/about', (req, res) => {
-  res.send(
-      `<h1>Halaman about</h1><hr>`
-    )
+app.get('/profil', (req, res) => {
+  res.render('profil')
 })
+
 app.listen(port, () => {
   console.log(` Aplikasi di port http://localhost:${port}`)
 })
