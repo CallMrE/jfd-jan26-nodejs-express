322d200bb8470df60514c8d19a1d183e1bd9b7e7
HanGieTime <elgykputra155@gmail.com>
2026-02-13 20:09:03 +0700

    test express

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(
+      `<h1>Halaman Utama</h1><hr>
+      <p>ini adalah halaman utama kita</p><hr>
+      <button>simpan</button>`
+    )
+})
+app.get('/about', (req, res) => {
+  res.send(
+      `<h1>Halaman about</h1><hr>`
+    )
+})
+app.listen(port, () => {
+  console.log(` Aplikasi di port http://localhost:${port}`)
+})
