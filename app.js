77eab7519212a9dac1bde6e9d3600f02580d7030
HanGieTime <elgykputra155@gmail.com>
2026-02-20 19:00:49 +0700

    tambah form tambah dan cara ambil isi form di back-end

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3005
 
 app.set('view engine','ejs') // setting penggunan tampate engine untuk express
 app.set('views','./view') //setting enunaan folder untuk menyimpan file .ejs
@@ -11,7 +11,38 @@ app.get('/', (req, res) => {
 app.get('/profil', (req, res) => {
   res.render('profil')
 })
-
+app.get('/pengalaman', (req, res) => {
+  let namaLengkap ='Si A'
+  // data harus di kirim dan di pangil ke view
+  res.render('detil-pengalaman',{
+    nama: namaLengkap,
+    alamat: 'Jakarta',
+    posisi: 'Programer',
+    perusahaan: 'PT. Sukses',
+    gaji: 9000000,
+    // pajak: (this.gaji > 10000000) ? 'kena pajak' : 'tidak perlu bayar pph'
+  })
+})
+// synchronous = berjalan berurutan
+// asynchronous = berjalan tidak berurutan
+app.get('/karyawan', async (req,res)=>{
+  res.render('karyawan/all', {
+       data_karyawan: await require('./model/m_karyawan').get_semua_karyawan()
+  })
+})
+app.get('/karyawan/detail/:id_kry', async (req,res)=>{
+  let id_kry = req.params.id_kry
+  res.render('karyawan/profil',{
+       profil_karyawan: await require('./model/m_karyawan').get_1_karyawan(id_kry)
+  })
+}),
+// app.get('/karyawan/hapus/:id_kry', async (req,res)=>{
+//   let id_kry = req.params.id_kry
+//   let proses_hapus = await require('./model/m_karyawan').hapus_1_karyawan(id_kry)
+//   if (proses_hapus.affectedRows > 0) {
+//         res.redirect('karyawan/all')
+//       }
+// })
 app.listen(port, () => {
-  console.log(` Aplikasi di port http://localhost:${port}`)
+  console.log(`Aplikasi di port http://localhost:${port}`)
 })
---
+++ model/m_karyawan.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_jan2026'
+})
+db.connect()
+
+module.exports =
+{
+  get_semua_karyawan: function() {
+    let sql = mysql.format(
+      'SELECT * FROM karyawan'
+    )
+    return new Promise( function(resolve,reject) {
+      db.query(sql, function(errorSql, hasil) {
+        if (errorSql) {              
+          reject(errorSql)
+        } else {
+          resolve(hasil)
+        }
+      })
+    })
+  },
+  get_1_karyawan: function (id_kry) {
+    let sql = mysql.format(
+      'SELECT * FROM karyawan WHERE id = ?',[id_kry]
+    )
+    return new Promise( function(resolve,reject) {
+      db.query(sql, function(errorSql, hasil) {
+        if (errorSql) {
+          reject(errorSql)
+        } else {
+          resolve(hasil)
+        }
+      })
+    })
+  },
+  hapus_1_karyawan: function (id_kry) {
+    let sql = mysql.format(
+      'DELETE * FROM karyawan WHERE id = ?',[id_kry]
+    )
+    return new Promise( function(resolve,reject) {
+      db.query(sql, function(errorSql, hasil) {
+        if (errorSql) {
+          reject(errorSql)
+        } else {
+          resolve(hasil)
+        }
+      })
+    })
+  }
+}
